a94d1bc4f8642ff5be2f25c97e4ddb110d12dba9
travoemily-fs <EATravo@student.fullsail.edu>
2024-11-16 11:58:10 -0800

    11/16 repo update
    final push for changes made to project. see CHANGELOG.md for detailed information on updates made.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,17 +14,27 @@
 - **11/14** - hosted fonts from adobe added to project
 - **11/14** - began set up of game interface by defining game container and drawing the first graphic inside index.js
 - **11/14** - added typescript to the project compile-time error checking
-- **11/14** - completed basic ping pong animation for app 
+- **11/14** - completed basic ping pong animation for app
+- **11/16** - fully implemented border change logic on collision
+- **11/16** - fully implemented promise when all 4 borders are collided with to end game
+- **11/16** - added mesh gradient background and /assets folder inside src to store
+- **11/16** - finalized css
 
 **CHANGED:**
 
 - **11/14** - used pixi.js cdn link to import into project
-- **11/14** - index.js to index.ts for typescript 
+- **11/14** - index.js to index.ts for typescript
+- **11/16** - added playAgainBtn to restart game after promise is fulfilled
 
 **FIXED:**
+
+- **N/A**
 
 **REMOVED:**
 
 - **11/14** - package files and node modules folder after changing pixi import
 
 **RESOURCES USED:**
+
+- **border collision guidance:** https://pixijs.com/8.x/playground?exampleId=advanced.collisionDetection
+- **using void in promises:** https://typescript.tv/new-features/the-void-operator-in-typescript-and-javascript/
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@
   const app = new PIXI.Application({
     width: 800,
     height: 500,
-    backgroundColor: 0x3398b9,
+    backgroundColor: 0x9D87F0,
   });
 
   // add to DOM
@@ -13,7 +13,7 @@
 
   // create graphic instance for circle 1
   const circle = new PIXI.Graphics();
-  circle.beginFill(0xf5ef42);
+  circle.beginFill(0x7F51E5);
   // draw your circle using x, y, and radius size
   circle.drawCircle(5, 5, 5);
   circle.endFill();
@@ -23,15 +23,76 @@
   let yv = 1;
   app.stage.addChild(circle);
 
-  // the ticker
-  app.ticker.add(() => {
-    if (circle.x >= 800 || circle.x <= 0) {
-      xv = -xv;
-    }
-    if (circle.y >= 500 || circle.y <= 0) {
-      yv = -yv;
-    }
-    circle.x += xv;
-    circle.y += yv;
+  // init canvas container
+  const canvas = document.querySelector("#mcontainer canvas") as HTMLElement;
+
+  // pre-determined colors for each border upon collision
+  const borderColors = {
+    top: "black",
+    bottom: "black",
+    left: "black",
+    right: "black",
+  };
+
+  // tracking border collisions for loop
+  const borderHits = {
+    top: false,
+    bottom: false,
+    left: false,
+    right: false,
+  };
+
+  // check if borders have been collided with
+  const allBordersHit = (): boolean =>
+    borderHits.top && borderHits.bottom && borderHits.left && borderHits.right;
+
+  // promise to handle game ending
+  const gameOverPromise = new Promise<void>((resolve) => {
+    // store ticker inside promise
+    app.ticker.add(() => {
+      // detect horizontal border collisions
+      if (circle.x >= 800 && !borderHits.right) {
+        xv = -xv;
+        canvas.style.borderRightColor = borderColors.right;
+        borderHits.right = true;
+      } else if (circle.x <= 0 && !borderHits.left) {
+        xv = -xv;
+        canvas.style.borderLeftColor = borderColors.left;
+        borderHits.left = true;
+      }
+
+      // detect vertical border collisions
+      if (circle.y >= 500 && !borderHits.bottom) {
+        yv = -yv;
+        canvas.style.borderBottomColor = borderColors.bottom;
+        borderHits.bottom = true;
+      } else if (circle.y <= 0 && !borderHits.top) {
+        yv = -yv;
+        canvas.style.borderTopColor = borderColors.top;
+        borderHits.top = true;
+      }
+
+      // check to see if all border hit values are true
+      if (allBordersHit()) {
+        resolve(); // resolve promise when all borders have been collided with
+      }
+
+      // update circle position
+      circle.x += xv;
+      circle.y += yv;
+    });
+  });
+  // game over logic after promise is resolved
+  gameOverPromise.then(() => {
+    app.ticker.stop(); // stops the game
+
+    // shows play again button
+    const playAgainBtn = document.getElementById("playAgain") as HTMLElement;
+    playAgainBtn.style.display = "block";
+
+    // restarts game when btn is clicked
+    playAgainBtn.addEventListener("click", () => {
+      location.reload();
+    });
   });
 })();
